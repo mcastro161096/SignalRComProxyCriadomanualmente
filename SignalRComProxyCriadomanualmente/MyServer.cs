11a1c2cad859c7d20525df24a7429e2cab49451d
Matheus Castro Oliveira <61395108+mcastro161096@users.noreply.github.com>
2021-09-07 16:13:19 -0300

    Adicionando classes base para o funcionanmento do signalR.

---
+++ SignalRComProxyCriadomanualmente/MyServer.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNet.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SignalRComProxyCriadomanualmente
+{
+    public class MyServer : Hub
+    {
+        public void AlertAll(string msg)
+        {
+            Clients.All.clientAlertAll(msg);
+        }
+    }
+}
