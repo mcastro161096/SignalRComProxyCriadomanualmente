27d2ef045278c0a32c121dd6a9ffbe02ecdd68ca
Matheus Castro Oliveira <61395108+mcastro161096@users.noreply.github.com>
2021-09-07 17:18:19 -0300

    Alterando projeto para torná-lo uma aplicação de desenho para multi-usuários.

---
+++ SignalRComProxyCriadomanualmente/Board.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNet.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SignalRComProxyCriadomanualmente
+{
+    public class Board : Hub
+    {
+        private const int BoardWidth = 250;
+        private const int BoardHeight = 250;
+        private static int[,] _buffer = GetEmpyBuffer();
+    }
+}
