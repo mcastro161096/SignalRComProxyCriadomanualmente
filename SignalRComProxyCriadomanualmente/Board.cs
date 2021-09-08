d28126e174aa43517390bacb828ee2eef1c8c609
Matheus Castro Oliveira <61395108+mcastro161096@users.noreply.github.com>
2021-09-08 07:36:24 -0300

    Implementados os métodos de desenho, limpeza e atualização no backend.

--- SignalRComProxyCriadomanualmente/Board.cs
+++ SignalRComProxyCriadomanualmente/Board.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 using System.Web;
 
 namespace SignalRComProxyCriadomanualmente
@@ -11,5 +12,38 @@ namespace SignalRComProxyCriadomanualmente
         private const int BoardWidth = 250;
         private const int BoardHeight = 250;
         private static int[,] _buffer = GetEmpyBuffer();
+
+        public Task BroadCastPoint(int x, int y)
+        {
+            x = x < 0 ? 0 : x;
+            x = x > BoardWidth ? x = BoardWidth - 1 : x;
+
+            y = y < 0 ? 0 : y;
+            y = y > BoardHeight ? y = BoardHeight - 1 : y;
+
+            int color = 0;
+            int.TryParse(Clients.Caller.color, out color);
+            _buffer[x, y] = color;
+            return Clients.Others.DrawPoint(x, y, Clients.Caller.color);
+        }
+
+        public Task BroadCastClear()
+        {
+            _buffer = GetEmpyBuffer();
+            return Clients.Others.Clear();
+        }
+
+        private static int[,] GetEmpyBuffer()
+        {
+            var buffer = new int[BoardWidth, BoardHeight];
+            return buffer;
+        }
+
+        public override Task OnConnected()
+        {
+            Clients.Caller.Update(_buffer);
+            return base.OnConnected();
+        }
+
     }
 }
