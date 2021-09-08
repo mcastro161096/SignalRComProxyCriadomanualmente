50210c921b2dbc3a07a11a65ced0a5c2279ec0c2
Matheus Castro Oliveira <61395108+mcastro161096@users.noreply.github.com>
2021-09-08 07:49:23 -0300

    Implementado cliente html.

---
+++ SignalRComProxyCriadomanualmente/Scripts/Board.js
@@ -0,0 +1,14 @@
+﻿//$(function () {
+        //    var connection = $.hubConnection();
+        //    var proxy = connection.createHubProxy("myServer");
+
+        //    connection.start().done(function () {
+        //        $("#sendAlert").click(function () {
+        //            proxy.invoke("AlertAll", "Opa");
+        //        });
+        //    });
+
+        //    proxy.on("clientAlertAll", function (msg) {
+        //        alert(msg);
+        //    });
+        //});
