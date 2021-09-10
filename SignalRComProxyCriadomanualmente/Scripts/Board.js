e3815c7c3a80d3c5a11c524eb0b740c91180283e
Matheus Castro Oliveira <61395108+mcastro161096@users.noreply.github.com>
2021-09-09 21:53:16 -0300

    Implementando canvas para desenhar.

--- SignalRComProxyCriadomanualmente/Scripts/Board.js
+++ SignalRComProxyCriadomanualmente/Scripts/Board.js
@@ -12,3 +12,15 @@
         //        alert(msg);
         //    });
         //});
+
+$(function () {
+    var colors = ["black", "red", "green", "blue", "yellow", "magenta", "white"];
+    var canvas = $("#cancas");
+    var colorElement = $("#color");
+    for (var i = 0; i < colors.length; i++) {
+        colorElement.append("<option value='" + (i + 1) + "'>" + colors[i] + "</li");
+    }
+
+
+
+});
