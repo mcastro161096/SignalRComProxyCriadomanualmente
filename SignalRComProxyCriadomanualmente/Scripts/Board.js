39bb71cb8c15b2fb319a2acdaca5f029c6d16d72
Matheus Castro Oliveira <61395108+mcastro161096@users.noreply.github.com>
2021-09-09 23:42:21 -0300

    JavaScript para fazer e apagar o desenho concluído.

--- SignalRComProxyCriadomanualmente/Scripts/Board.js
+++ SignalRComProxyCriadomanualmente/Scripts/Board.js
@@ -15,12 +15,37 @@
 
 $(function () {
     var colors = ["black", "red", "green", "blue", "yellow", "magenta", "white"];
-    var canvas = $("#cancas");
+    var canvas = $("#canvas");
     var colorElement = $("#color");
     for (var i = 0; i < colors.length; i++) {
         colorElement.append("<option value='" + (i + 1) + "'>" + colors[i] + "</li");
     }
 
+    var buttonPressed = false;
+    canvas.mousedown(function () {
+        buttonPressed = true;
+    }).mouseup(function () {
+        buttonPressed = false;
+    }).mousemove(function (e) {
+        if (buttonPressed)
+        {
+            setPoint(e.offsetX, e.offsetY, colorElement.val());
+        }
+    });
 
+    var context = canvas[0].getContext("2d");
+
+    function setPoint(x, y, color) {
+        context.fillStyle = colors[color - 1];
+        context.beginPath();
+        context.arc(x, y, 2, 0, Math.PI * 2);
+        context.fill();
+    }
+
+    function clearPoints() {
+        context.clearRect(0, 0, canvas.width(), canvas.height());
+    }
+
+    $("#clear").click(clearPoints);
 
 });
